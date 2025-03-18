60dc748184f00ec4eade38e9ebfa180cdb9cbc8b
7i7o <mparij@communitylabs.com>
2025-03-18 19:16:58 +0100

    fix: add css injected by js vite plugin

--- frameworks/react-legacy/vite.config.ts
+++ frameworks/react-legacy/vite.config.ts
@@ -2,24 +2,25 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import path from "node:path";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
-  plugins: [react(), dts({ insertTypesEntry: true })],
+  plugins: [react(), dts({ insertTypesEntry: true }), cssInjectedByJsPlugin()],
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
       name: "arweave-wallet-kit",
       formats: ["es", "umd"],
-      fileName: (format) => `index.${format}.js`
+      fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
       external: ["react", "react-dom", "arweave", "@othent/kms"],
       output: {
         globals: {
           react: "React",
-          "react-dom": "ReactDOM"
-        }
-      }
-    }
-  }
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
 });
